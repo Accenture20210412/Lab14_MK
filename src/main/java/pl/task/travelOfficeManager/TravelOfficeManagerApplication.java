38a1d2f034153c576e27d9e436f76d395e0df963
Malgorzata <grosiaczysko@gmail.com>
2021-04-23 11:23:52 +0200

    First commit

---
+++ src/main/java/pl/task/travelOfficeManager/TravelOfficeManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.task.travelOfficeManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelOfficeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelOfficeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/task/travelOfficeManager/TravelOfficeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.task.travelOfficeManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelOfficeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
